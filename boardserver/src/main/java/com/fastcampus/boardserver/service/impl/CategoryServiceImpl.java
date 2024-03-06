04f2dc820fdfff3c86ccff509904e42c4679ac1f
devhak <devhak.enzo@gmail.com>
2024-03-06 22:11:41 +0900

    lopg4j2 dependency 추가

--- boardserver/src/main/java/com/fastcampus/boardserver/service/impl/CategoryServiceImpl.java
+++ boardserver/src/main/java/com/fastcampus/boardserver/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,13 @@
 package com.fastcampus.boardserver.service.impl;
 
 import com.fastcampus.boardserver.dto.CategoryDTO;
+import com.fastcampus.boardserver.exception.BoardServerException;
 import com.fastcampus.boardserver.mapper.CategoryMapper;
 import com.fastcampus.boardserver.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +21,12 @@ public class CategoryServiceImpl implements CategoryService {
     public void register(String accountId, CategoryDTO categoryDTO) {
         if (accountId != null) {
             categoryMapper.register(categoryDTO);
+            try {
+                categoryMapper.register(categoryDTO);
+            } catch (RuntimeException e) {
+                log.error("register 실패");
+                throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            }
         } else {
             log.error("register ERROR! {}", categoryDTO);
             throw new RuntimeException("register ERROR! 상품 카테고리 등록 메서드를 확인해주세요\n" + "Params : " + categoryDTO);
@@ -29,7 +37,12 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void update(CategoryDTO categoryDTO) {
         if (categoryDTO != null) {
-            categoryMapper.updateCategory(categoryDTO);
+            try {
+                categoryMapper.updateCategory(categoryDTO);
+            } catch (RuntimeException e) {
+                log.error("update 실패");
+                throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            }
         } else {
             log.error("update ERROR! {}", categoryDTO);
             throw new RuntimeException("update ERROR! 물품 카테고리 변경 메서드를 확인해주세요\n" + "Params : " + categoryDTO);
@@ -39,7 +52,12 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void delete(int categoryId) {
         if (categoryId != 0) {
-            categoryMapper.deleteCategory(categoryId);
+            try {
+                categoryMapper.deleteCategory(categoryId);
+            } catch (RuntimeException e) {
+                log.error("delete 실패");
+                throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            }
         } else {
             log.error("deleteCategory ERROR! {}", categoryId);
             throw new RuntimeException("deleteCategory ERROR! 물품 카테고리 삭제 메서드를 확인해주세요\n" + "Params : " + categoryId);
